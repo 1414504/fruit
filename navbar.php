92148b05bfa3c05487fae1ba3746c8b2b0f23f49
1414504 <1414504@rgu.ac.uk>
2016-08-17 19:30:49 +0100

    yes

--- navbar.php
+++ navbar.php
@@ -1,22 +1,22 @@
 <div class="navbar">
     <div class="row">
-        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white; width: 100%; text-align: left;" href="showfoods.php">All food Items</a></h5></div>
+        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white; width: 100%; text-align: left;" href="showfood.php">All food Items</a></h5></div>
 
     </div>
     <div class="row">
-        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white; width: 100%;text-align: left;" href="show-category-foods.php?cat=vegetarian">Vegetarian foods</a></h5></div>
+        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white; width: 100%;text-align: left;" href="show-category-food.php?cat=vegetarian">Vegetarian foods</a></h5></div>
 
     </div>
     <div class="row">
-        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="show-category-foods.php?cat=nonveg">Non Veg foods</a></h5></div>
+        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="show-category-food.php?cat=nonveg">Non Veg foods</a></h5></div>
 
     </div>
     <div class="row">
-        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="show-category-foods.php?cat=animal">Animal foods</a></h5></div>
+        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="show-category-food.php?cat=animal">Animal foods</a></h5></div>
 
     </div>
     <div class="row">
-        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="addfoods.php">Insert food</a></h5></div>
+        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="addfood.php">Insert food</a></h5></div>
 
     </div>
 
